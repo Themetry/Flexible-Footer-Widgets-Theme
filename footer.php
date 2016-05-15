9991ea0659536d627ff855c3f2360d2eb320e37b
Leland Fiegel <lelandf@gmail.com>
2016-05-15 13:19:42 -0400

    Widget stylings

--- footer.php
+++ footer.php
@@ -14,10 +14,30 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
+		<?php if ( is_active_sidebar( 'footer-1' ) || is_active_sidebar( 'footer-2' ) || is_active_sidebar( 'footer-3' ) ) : ?>
+			<div class="footer-widgets">
+				<?php if ( is_active_sidebar( 'footer-1' ) ) : ?>
+					<div class="footer-widget-area">
+						<?php dynamic_sidebar( 'footer-1' ); ?>
+					</div>
+				<?php endif; ?>
+				<?php if ( is_active_sidebar( 'footer-2' ) ) : ?>
+					<div class="footer-widget-area">
+						<?php dynamic_sidebar( 'footer-2' ); ?>
+					</div>
+				<?php endif; ?>
+				<?php if ( is_active_sidebar( 'footer-3' ) ) : ?>
+					<div class="footer-widget-area">
+						<?php dynamic_sidebar( 'footer-3' ); ?>
+					</div>
+				<?php endif; ?>
+			</div><!-- .footer-widgets -->
+		<?php endif; ?>
+
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'flexible-footer-widgets' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'flexible-footer-widgets' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'flexible-footer-widgets' ), 'flexible-footer-widgets', '<a href="https://themetry.com" rel="designer">Themetry</a>' ); ?>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'flexible-footer-widgets' ), 'Flexible Footer Widgets', '<a href="https://themetry.com" rel="designer">Themetry</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
--- functions.php
+++ functions.php
@@ -24,6 +24,15 @@ function flexible_footer_widgets_setup() {
 	 */
 	load_theme_textdomain( 'flexible-footer-widgets', get_template_directory() . '/languages' );
 
+
+	/*
+	 * Let WordPress manage the document title.
+	 * By adding theme support, we declare that this theme does not use a
+	 * hard-coded <title> tag in the document head, and expect WordPress to
+	 * provide it for us.
+	 */
+	add_theme_support( 'title-tag' );
+
 	/*
 	 * Switch default core markup for search form, comment form, and comments
 	 * to output valid HTML5.
